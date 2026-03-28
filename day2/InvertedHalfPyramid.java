fff08c534d93ca026dcac83ef7124eaf46a826c8
prem-singh333 <premkumarsingh7241@gmail.com>
2026-03-28 11:13:52 +0530

    Day 2 - Solved the Inverted Half Pyramid Pattern

---
+++ day2/InvertedHalfPyramid.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class InvertedHalfPyramid {
+    public static void main(String args[]){
+        //Q no.10 Print inverted half pyramid pattern
+
+        // * * * * *
+        // * * * *
+        // * * *
+        // * *
+        // *
+
+        int n = 5;
+
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
